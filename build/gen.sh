b0cf06b38ca93ba4d75889e8d52e40533359e477
Dmitriy Govorukhin <dgovorukhin@samokat.ru>
2023-06-19 22:43:13 +0200

    build + up wireguard

--- build/gen.sh
+++ build/gen.sh
@@ -12,6 +12,13 @@ docker run -i -d --rm --name $container_name $image_name:latest
 echo "copy artifacts from build container"
 docker cp $container_name:/build/wg0.conf ../wireguard-ui/config/wg0.conf
 docker cp $container_name:/build/pihole.conf ../pi-hole/config/pihole.conf
+docker cp $container_name:/build/docker-compose.yaml ../docker-compose.yaml
+
+chmod o-r ../wireguard-ui/config/wg0.conf
+chmod g-r ../wireguard-ui/config/wg0.conf
+
+chmod o-r ../pi-hole/config/pihole.conf
+chmod g-r ../pi-hole/config/pihole.conf
 
 echo "stop build container"
 docker stop $container_name
